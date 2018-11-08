4d20ac493cd7b5b3b7ddba1f4b13c1deffb89359
trottar <trottar@jlab.org>
2018-11-08 08:32:56 -0500

    Forgot a file

---
+++ elastics/run_EvtNum.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+#Which spectrometer
+spec=$1
+#spec=${0##*_}
+#spec=${spec%%.sh}
+SPEC=$(echo "$spec" | tr '[:lower:]' '[:upper:]')
+
+#Input run numbers
+inputFile="inputRuns"
+outputFile="outputGrep"
+
+while IFS='' read -r line || [[ -n "$line" ]];
+do
+    echo "Run number read from file: $line"
+
+#Which run
+runNum=$line
+
+#Number of events
+numEvts=-1
+
+#Which scripts to run
+script="get_EvtNum_${spec}.C"
+scriptPlots="plot_Stat_${spec}.C"
+
+#which commands to run
+runScript="root -l -b -q \"${script}(${runNum},${numEvts})\""
+runScriptPlots="root -l -b -q \"${scriptPlots}(${runNum},${numEvts})\""
+
+#Excecute 
+{
+
+echo "Running ${script}"
+echo "Getting ${numEvts} number of events for run ${runNum} for ${SPEC}"
+eval ${runScript}
+eval ${runScriptPlots}
+sleep 2
+grep "Run #" /home/trottar/ResearchNP/ROOTAnalysis/REPORT_OUTPUT/COIN/PRODUCTION/replay_coin_production_${runNum}_${numEvts}.report >> outputGrep
+grep -i BCM4B /home/trottar/ResearchNP/ROOTAnalysis/REPORT_OUTPUT/COIN/PRODUCTION/replay_coin_production_${runNum}_${numEvts}.report >> outputGrep
+
+}
+
+done < "$inputFile"
+
+mv *.png OUTPUT/pngFiles/
+mv $outputFile OUTPUT/
+
+
